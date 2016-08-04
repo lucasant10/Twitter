3813df5356a8e8aed4150bb1d2d8fc210b98d751
lucasant10 <lucasant@gmail.com>
2016-08-04 15:19:17 -0300

    conserto bug ao criar o arquivo pck, e adicao do metodo save_tfidf

--- tfidf.py
+++ tfidf.py
@@ -13,16 +13,20 @@ class TfIdf():
     tp = TextProcessor()
 
     def tf(self, word, w_counter):
-        return w_counter[word] / len(w_counter)
+        return (w_counter[word] / float(len(w_counter)))
 
     def n_containing(self, word, doc_counter):
-        return sum(1 for w_counter in doc_counter if word in w_counter)
+        count = 0
+        for document in doc_counter:
+            if document[word] > 0:
+                count += 1
+        return count
 
     def idf(self, word, doc_counter):
-        return math.log(len(doc_counter) / (1 + self.n_containing(word, doc_counter)))
+        return (math.log(len(doc_counter) / float(self.n_containing(word, doc_counter))))
 
     def tfidf(self, word, w_counter, doc_counter):
-        return self.tf(word, w_counter) * self.idf(word, doc_counter)
+        return (self.tf(word, w_counter) * self.idf(word, doc_counter))
 
     def read_files(self, dir_in):
         doc_list=list()
@@ -40,8 +44,7 @@ class TfIdf():
 
         return tw_files, count_list
 
-    def save_counter(self, dir_in,dir_out):
-        doc_list=list()
+    def save_counters(self, dir_in,dir_out):
         tw_files = ([file for root, dirs, files in os.walk(dir_in)
             for file in files if file.endswith('.json') ])
         # lista os arquivos ja processados
@@ -51,6 +54,7 @@ class TfIdf():
         tw_files = [x for x in tw_files if x.split('.')[0]+".pck" not in pck]
         for i,tw_file in enumerate(tw_files):
             print("processando o arquivo: "+tw_files[i]+"\n")
+            doc_list=list()
             with open(dir_in+tw_file) as data_file:
                 for line in data_file:
                     tweet = json.loads(line)
@@ -61,32 +65,36 @@ class TfIdf():
             with open(dir_out+tw_files[i].split('.')[0]+".pck", 'wb') as handle:
                 pickle.dump(t_count, handle)
 
+    def save_tfidf(self, dir_in,dir_out):
+        counter_list = list()
+        tot_counter = Counter()
+        pck = ([file for root, dirs, files in os.walk(dir_in)
+            for file in files if file.endswith('.pck') ])
+        for i,counter_file in enumerate(pck):
+            print("processando o arquivo: "+counter_file+"\n")
+            with open(dir_in+counter_file, 'rb') as data_file:
+                tw_counter = pickle.load(data_file)
+                tot_counter += tw_counter
+                counter_list.append(tw_counter)
+        for i, w_counter in enumerate(counter_list):
+            print("gerando o tfidf de "+pck[i])
+            dict_term = {word: self.tfidf(word, w_counter, counter_list) for word in tot_counter}
+            dic = {k:v for k,v in dict_term.items() if v>0}
+            sort = sorted(dic.items(), key=lambda x: x[1], reverse=True)
+            print("salvando o arquivo "+pck[i])
+            with open(dir_out+pck[i]+".txt", "w") as f:
+             f.write(json.dumps(sort))
+             f.close()
+            
 
 if __name__=='__main__':
 
-    dir_in = "/Users/lucasso/Documents/tweets_pedro/"
-    dir_out = "/Users/lucasso/Dropbox/Twitter_Marcelo/Report/Tf-Idf/Counter_dep/"
+    dir_in = "/Users/lucasso/Documents/pck/"
+    dir_out = "/Users/lucasso/Documents/pck/"
     ptbr = PtBrTwitter(dir_in,dir_out)
     tfidf = TfIdf()
-    tfidf.save_counter(dir_in,dir_out)
-
-
-    """
-    for c in count_list:
-        tot_counter += c
-
-    for i, w_counter in enumerate(count_list):
-        dict_term = {word: tfidf.tfidf(word, w_counter, count_list) for word in tot_counter}
-        dic = {k:v for k,v in dict_term.items() if v<0}
-        sort = sorted(dic.items(), key=lambda x: x[1])
-        with open(dir_out+tw_files[i]+".pck", "w") as f: f.write(json.dumps(sort))
-    """
-
-    
-
-
-
-
+    #tfidf.save_counters(dir_in,dir_out)
+    tfidf.save_tfidf(dir_in, dir_out)
 
 
 
@@ -104,7 +112,7 @@ if __name__=='__main__':
     for i, w_counter in enumerate(doc_counter):
 
         dict_term = {word: tfidf.tfidf(word, w_counter, doc_counter) for word in tot_counter}
-        dic = {k:v for k,v in dict_term.items() if v<0}
+        dic = {k:v for k,v in dict_term.items() if v>0}
         sort = sorted(dic.items(), key=lambda x: x[1])
         with open(dir_out+"doc_"+str(i)+".txt", "w") as f: f.write(json.dumps(sort))
 
