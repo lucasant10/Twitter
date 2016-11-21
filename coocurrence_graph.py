9907c09d39b543e030e70fb8b92932bb551db755
lucasant10 <lucasant@gmail.com>
2016-11-21 17:38:10 -0300

    alteracao do grafo de coocorrencia

--- coocurrence_graph.py
+++ coocurrence_graph.py
@@ -2,22 +2,19 @@ import nltk
 from nltk.collocations import *
 import os
 import pickle
-import numpy as np
 from text_processor import TextProcessor
 import json
-import itertools
 import pickle
-from word_table import WordTable
 import networkx as nx
 import re
-from sklearn.feature_extraction.text import CountVectorizer
 
 
 
-def load_word_list(dir_in, file_name):
+
+def load_file(dir_in, file_name):
     with open(dir_in+file_name, 'rb') as handle:
-            word_list = pickle.load(handle)
-            return word_list
+            fl = pickle.load(handle)
+            return fl
 
 
 def get_bigrams(tokens):
@@ -40,31 +37,56 @@ def load_files(dir_in):
 
 if __name__=='__main__':
 
-    dir_in= "/home/lucasso/Documents/pck/"
-    dir_ent = "/home/lucasso/Documents/tweets_pedro/"
-    dir_out= "/home/lucasso/Documents/"
+    dir_in= "/Users/lucasso/Documents/pck/"
+    dir_ent = "/Users/lucasso/Documents/tweets_pedro/"
+    dir_out= "/Users/lucasso/Dropbox/Twitter_Marcelo/Report/plot/"
     doc_list = load_files(dir_ent)
     tp = TextProcessor()
     tweets = tp.text_process(doc_list)
-    word_list = set(load_word_list(dir_out,"word_list.pck"))
+    word_list = set(load_file(dir_out,"word_list.pck"))
     #lista já processada sem entropia 0 e ration >1. remove todas as outras palavras que não interessam dos tweets
     tweets =[[i for i in t if i in word_list] for t in tweets] 
     hashtags = re.compile(r"""(?:\#+[\w_]+[\w\'_\-]*[\w_]+)""")
     hs_set =set()
 
     hastgs_list = list()
-    hastgs_list2 = list()
     for tweet in tweets:
-        hastgs_list.append( ','.join(hashtags.findall( ' '.join(tweet))))
+        v = ','.join(hashtags.findall( ' '.join(tweet)))
+        l = hashtags.findall( ' '.join(tweet))
+        hastgs_list.append(l)
         hs_set |= set(v.split(","))
 
-    hastgs_list = [e for e in hastgs_list if e] # remove as listas em branco
-    vectorizer = CountVectorizer()
-    matrix = vectorizer.fit_transform(hashtags)
-#processa o grafo
+    hastgs_list = [e for e in hastgs_list if e] # remove as listas em branco     
+    
+    tweets =[[i for i in t if i not in hs_set] for t in tweets] # remove as hashtags dos tweets 
+
+    #with open(dir_out+"hashtag_set.pck", 'wb') as handle:
+    #    pickle.dump(hs_set, handle)
+    #with open(dir_out+"hashtag_list.pck", 'wb') as handle:
+    #    pickle.dump(hastgs_list, handle)
+    #with open(dir_out+"tweeets_n_hstgs.pck", 'wb') as handle:
+    #    pickle.dump(tweets, handle)
+
+    #processa o grafo de hashtags
 
     graph = nx.DiGraph()
-    for tweet in tw:
+    for hashtags in hastgs_list:
+        for i in range(len(hashtags)-1):
+            first = hashtags[i]
+            second = hashtags[i+1]
+            if graph.has_edge(first,second):
+                weight = graph.get_edge_data(first,second)['weight'] +1
+                graph.edge[first][second]['weight']=weight
+            else:
+                graph.add_edge(first,second,weight=1)
+
+    nx.write_gml(graph,dir_out+"grafo_hastags.gml")
+
+
+    #processa o grafo de palavras
+
+    graph = nx.DiGraph()
+    for tweet in tweets:
         for i in range(len(tweet)-1):
             first = tweet[i]
             second = tweet[i+1]
@@ -75,9 +97,3 @@ if __name__=='__main__':
                 graph.add_edge(first,second,weight=1)
 
     nx.write_gml(graph,dir_out+"grafo_direcionado2.gml")
-    pos=nx.spring_layout(graph)
-    weights = nx.get_edge_attributes(graph,'weight')
-    nx.draw_networkx_nodes(graph,pos,node_size=150)
-    nx.draw_networkx_edge_labels(graph,pos,edge_labels=weights)
-    nx.draw_networkx_edges(graph,pos)
-    plt.show()
