ee84c215df5d494e08e8307da1bc623d77227f3c
= <=>
2017-11-23 20:51:09 +0000

    changes for mongodb

--- ICWSM/time_distribution.py
+++ ICWSM/time_distribution.py
@@ -9,28 +9,12 @@ import codecs
 from collections import Counter
 import numpy as np
 from matplotlib import pyplot as plt
+import pymongo
 
 def month_tw(time):
     first = 1380585600000
     month= 2419200000
     return math.ceil((time - first)/month)
-
-def load_files(dir_in):
-    doc_list = list()
-    tw_files = sorted([file for root, dirs, files in os.walk(dir_in)
-                 for file in files if file.endswith('.json')])
-    # 10/2013 1380585600
-    # 10/2017 1506816000
-    for tw_file in tw_files:
-        temp = list()
-        with open(dir_in+tw_file) as data_file:
-            for line in data_file:
-                tweet = json.loads(line)
-                time = int(tweet['created_at'])
-                if(1380585600000 <= time <= 1506816000000 ):
-                    temp.append(tweet)
-        doc_list.append(temp)
-    return doc_list, tw_files
 
 def save_file(path, filename, dic_file):
     for dirs, tweets in dic_file.items():
@@ -41,13 +25,14 @@ def save_file(path, filename, dic_file):
         f.close()
 
 def prepare_text(counter, samples):
-    l = list(counter.items())
-    arr = np.asarray([x[0] for x in l])
+    l = list(sorted(count.items(), key=lambda i: i[0]))
+    arr = np.asarray([x[1] for x in l])
     arr = arr/sum(arr) * samples
     txt = ''
-    for i, v in enumerate(num):
+    for i, v in enumerate(arr):
         txt += 'month %s : %s \n' %((i+1), int(v)) 
     return txt
+
 
 if __name__ == "__main__":
     
@@ -56,25 +41,18 @@ if __name__ == "__main__":
     path = dict(cf.items("file_path"))
     dir_in = path['dir_in']
 
-    count = Counter()
-    tweet = list()
-    doc_list, tw_files = load_files(dir_in)
-    for i, tweet in enumerate(doc_list):
-        temp = dict()
-        for tw in tweet:
-            month = month_tw(int(tw['created_at']))
-            count[month] += 1
-            path =  dir_in + str(month) + os.path.sep
-            if path not in temp:
-                temp[path] = [tw]
-            else:
-                temp[path].append(tw)
-        save_file(path, tw_files[i], temp)
+    client = pymongo.MongoClient("mongodb://localhost:27017")
+    db = client.twitterdb
+    tweets = db.tweets.find({'created_at' : {'$gte':1380585600000, '$lt':1506816000000}})
     
+    count = Counter()
+    for tweet in tweets:
+        month = month_tw(int(tweet['created_at']))
+        count[month] += 1
+
     np.save(dir_in+'tw_counter_distribution.npy', count) 
     with open(dir_in + 'samples_per_folder.txt', 'w') as out:
         out.write(prepare_text(count, 1000))
-
     labels, values = zip(*sorted(count.items(), key=lambda i: i[0]))
     indexes = np.arange(len(labels))
     width = 1
--- import_tw_db.py
+++ import_tw_db.py
@@ -3,28 +3,48 @@ import pymongo
 import configparser
 import os
 from text_processor import TextProcessor
-    
+import xlrd
 cf = configparser.ConfigParser()
 cf.read("file_path.properties")
 path = dict(cf.items("file_path"))
 dir_in = path['dir_in']
+dir_xls = path['dir_xls']
 
 client = pymongo.MongoClient("mongodb://localhost:27017")
 db = client.twitterdb
-
-tw_files = sorted([file for root, dirs, files in os.walk(dir_in)
-             for file in files if file.endswith('.json')])
 tp = TextProcessor()
-for tw_file in tw_files:
-    with open(dir_in+tw_file) as data_file:
-        print('file %s' % data_file)
-        for line in data_file:
-            tweet = json.loads(line)
-            tweet['text_processed'] = ' '.join(tp.text_process([tweet['text']], text_only=True)[0])
-            tweet['cond_55'] = 'nao_eleitos'
-            db.tweets.insert(tweet)
-            
-            
+tw_files = sorted([file for root, dirs, files in os.walk(dir_in)
+        for file in files if file.endswith('.json')])
+excel = True
 
+if excel:
 
+    sheet_name = "nao_eleitos"
+    col = 4
+    rep_dic = {}
+    for fname in tw_files:
+        rep_dic[fname.split('_',1)[0]] = fname
+    xls = xlrd.open_workbook(dir_xls)
+    sheet = xls.sheet_by_name(sheet_name)
+    for i in range(sheet.nrows):
+        id_rep = str(int(sheet.cell_value(rowx= i, colx=col)))
+        if (id_rep in rep_dic):
+            with open(dir_in+rep_dic[id_rep]) as data_file:
+                print('file %s' % data_file)
+                for line in data_file:
+                    tweet = json.loads(line)
+                    tweet['text_processed'] = ' '.join(tp.text_process([tweet['text']], text_only=True)[0])
+                    tweet['cond_55'] = sheet_name
+                    db.tweets.insert(tweet)
 
+else:                    
+                
+    for tw_file in tw_files:
+        with open(dir_in+tw_file) as data_file:
+            print('file %s' % data_file)
+            for line in data_file:
+                tweet = json.loads(line)
+                tweet['text_processed'] = ' '.join(tp.text_process([tweet['text']], text_only=True)[0])
+                tweet['cond_55'] = 'nao_eleitos'
+                db.tweets.insert(tweet)
+                
